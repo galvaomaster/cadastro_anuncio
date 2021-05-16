003998cf9570a50555c7086b2c4983794083e23e
anderson.galvao <anderson.galvao@partners.mv.com.br>
2021-05-16 14:56:09 -0300

    Entrega do Desafio

--- ad_registration.py
+++ ad_registration.py
@@ -1,58 +1,56 @@
 import json
 import os
-from datetime import  datetime
+from datetime import datetime
+from os.path import isfile
 
 
-json_file = {"ad_name": " ", 
-            "customer": "", 
-            "start_date": "", 
-            "end_date": "", 
-            "value": ""}
+json_file = {"ad_name": " ",
+             "customer": "",
+             "start_date": "",
+             "end_date": "",
+             "value": ""}
 
-#escreve um arquivo .json
+
 def read_json():
     json_file = {}
     if os.path.exists('ad.json'):
-      with open('ad.json', 'r') as f:
-        json_file = json.load(f)
-        return json_file
-
+        with open('ad.json', 'r', encoding='utf-8') as f:
+          json_file = json.load(f)
+        
+    return json_file
 
 def create_json(json_file):
   with open('ad.json', 'w') as f:
-    json.dump(json_file, f, separators=(',', ':'))
-
-  
+    json.dump(json_file, f)
 
 
-json_file = read_json()
 comand = 1
-while comand != 0: 
-  print("                     ")
-  comand = int(input("Deseja Cadastrar um Anúncio?  [1]-Sim  [0]-Não "))
+while comand != 0:
+    print("                     ")
+    comand = int(input("Deseja Cadastrar um Anúncio?  [1]-Sim  [0]-Não "))
 
-  if comand == 1:
-    ad_name = input("Digite o nome do Anúncio: ").strip()
-    customer = input("Digite o nome do Cliente: ").strip()
-    
-    start_str_date = input("Digite a data de início do anuncio no formato YYYY/MM/DD: ")
-    start_date = datetime.strptime(start_str_date, '%Y/%m/%d').date()
-        
-    end_str_date = input("Digite a data fim do anuncio no formato YYYY/MM/DD: ")
-    end_date = datetime.strptime(end_str_date, '%Y/%m/%d').date()
-    
-    value = float(input("Digite o valor do Anúncio: ").strip())
+    if comand == 1:
+        ad_name = input("Digite o nome do Anúncio: ").strip()
+        customer = input("Digite o nome do Cliente: ").strip()
 
-    # start_str_date = start_date.strftime('%Y/%m/%d')
-    # end_str_date = end_date.strftime('%Y/%m/%d')
+        start_str_date = input(
+            "Digite a data de início do anuncio no formato YYYY/MM/DD: ")
+        start_date = datetime.strptime(start_str_date, '%Y/%m/%d').date()
 
-    json_file = {
-      "ad_name": ad_name,
-      "customer": customer,
-      "start_date": start_date,
-      "end_date": end_date,
-      "value": value
-    }
-    create_json(json_file)
+        end_str_date = input(
+            "Digite a data fim do anuncio no formato YYYY/MM/DD: ")
+        end_date = datetime.strptime(end_str_date, '%Y/%m/%d').date()
 
+        value = float(input("Digite o valor do Anúncio: ").strip())
 
+        # start_str_date = start_date.strftime('%Y/%m/%d')
+        # end_str_date = end_date.strftime('%Y/%m/%d')
+
+        json_file = {
+            "ad_name": ad_name,
+            "customer": customer,
+            "start_date": start_date,
+            "end_date": end_date,
+            "value": value
+        }
+create_json(json_file)
--- calculadora.py
+++ calculadora.py
@@ -1,27 +1,27 @@
 
-def cadastro_anuncio():
-    valor_investido = float(input("Digite o valor Investido: "))
+valor_investido = float(input("Digite o valor Investido: "))
 
-    # cliques = 0.0
-    compartilhamento  = 0.0
-    #Este ponto aqui para o primeiro desafio não coloquei pq acho que não precisa
-    #Para cada 100 pessoas que visualizam o anúncio 12 clicam nele.
-    #cliques = pessoas * 0.12
-    #cliques =  100 / 12
+cliques = 0.0
+pessoas = 0
+compartilhamento = 0.0
+# Este ponto aqui para o primeiro desafio não coloquei pq acho que não precisa
+# Para cada 100 pessoas que visualizam o anúncio 12 clicam nele.
 
-    # //a cada 20 pessoas que clicam no anúncio 3 compartilham nas redes sociais
+cliques =  100 / 12
 
-    compartilhamento = 20 *  3
+# //a cada 20 pessoas que clicam no anúncio 3 compartilham nas redes sociais
 
-    #cada compartilhamento nas redes sociais gera 40 novas visualizações.
-    #o mesmo anúncio é compartilhado no máximo 4 vezes em sequência 
-    visualizacoes_compartilhada = (compartilhamento * 40) /4
+compartilhamento = (20 * cliques) * 3
 
-    #30 pessoas visualizam o anúncio original (não compartilhado) a cada R$ 1,00 investido.
+# cada compartilhamento nas redes sociais gera 40 novas visualizações.
+# o mesmo anúncio é compartilhado no máximo 4 vezes em sequência
+visualizacoes_compartilhada = (compartilhamento * 40) / 4
 
-    visualizacoes_original = valor_investido * 30
+# 30 pessoas visualizam o anúncio original (não compartilhado) a cada R$ 1,00 investido.
 
+visualizacoes_original = valor_investido * 30
 
-    print('Para o valor Investido de R$ {}, anúncio original irá alcançar {} Pessoas'.format(valor_investido,visualizacoes_original))
-    print('Para o valor Investido de R$ {}, o anúncio compartilhado irá alcançar {:.2f} Pessoas'.format(valor_investido,visualizacoes_compartilhada))
-
+print('Para o valor Investido de R$ {}, anúncio original irá alcançar {} Pessoas'.format(
+    valor_investido, visualizacoes_original))
+print('Para o valor Investido de R$ {}, o anúncio compartilhado irá alcançar {:.2f} Pessoas'.format(
+    valor_investido, visualizacoes_compartilhada))
